b8d450f7fd6caab25056beaedbe67e0173133187
eyberthrojas <eyberthrojas@gmail.com>
2020-11-09 21:10:25 -0500

    Funcionando

--- GUI.py
+++ GUI.py
@@ -6,15 +6,24 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import numpy as np
 import cv2 as cv
+import tensorflow as tf
+from tensorflow import keras
+
 class App(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        physical_devices = tf.config.experimental.list_physical_devices('GPU') 
+        for physical_device in physical_devices: 
+            print(physical_device)
+            tf.config.experimental.set_memory_growth(physical_device, True)
+        tf.keras.backend.set_floatx('float64')
         self.title = 'GUI Demo AUTOMATAS'
         self.width = 1040
         self.height = 720
         self.left = 50
         self.top = 50 
         self.img = []
+        self.model = keras.models.load_model('modelos/modelo1.h5')
         self.initUI()
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -23,14 +32,15 @@ class App(QtWidgets.QMainWindow):
         # Título 
         titulo = QtWidgets.QLabel(self)
         titulo.setAlignment(QtCore.Qt.AlignCenter)
-        titulo.setStyleSheet('border: gray; border-style:solid; border-width: 1px; color: black; font: 20pt Arial')
+        titulo.setStyleSheet('border: red; border-style:solid; border-width: 5px; color: black; font: 20pt Arial')
         titulo.setText("Predictor MNINST")
         # Grafica 
         self.canvas = Canvas(self, width = 8, height = 4)
-        self.canvas.setStyleSheet('border: gray; border-style:solid; border-width: 1px')
+        self.canvas.setStyleSheet('border: blue; border-style:solid; border-width: 5px')
         # Boton Inicio
         self.boton_predecir = QtWidgets.QPushButton(self)
         self.boton_predecir.setText("Predecir digito")
+        self.boton_predecir.setStyleSheet('border: red; border-style:solid; border-width: 5px; color: blue; font: 15pt Arial')
         self.boton_predecir.clicked.connect(self.predecir)
         # Visualización
         self.prediccion = QtWidgets.QLabel(self)
@@ -39,8 +49,8 @@ class App(QtWidgets.QMainWindow):
         
         # Layouts
         self.controles = QtWidgets.QHBoxLayout(self)
-        self.controles.addWidget(self.boton_predecir)
-        self.controles.addWidget(self.prediccion)
+        self.controles.addWidget(self.boton_predecir,30)
+        self.controles.addWidget(self.prediccion,70)
         # Crea layoud raiz
         self.raiz = QtWidgets.QVBoxLayout(self)
         self.raiz.addWidget(titulo,5)
@@ -57,12 +67,10 @@ class App(QtWidgets.QMainWindow):
         self.canvas.plot(ima_resize)
         ima_resize = ima_resize.T
         ima_reshape = ima_resize.reshape([1,400])
-        #pred = model.predict(ima_reshape,verbose=0)
-        self.prediccion.setText("No se")
+        pred = np.argmax(self.model.predict(ima_reshape,verbose=0))
+        self.prediccion.setText("El digito escrito correspone a un {}".format(str(pred)))
         
     
-            
-
 class Canvas(FigureCanvas):
     def __init__(self, parent = None, width = 5, height = 5, dpi = 100):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
